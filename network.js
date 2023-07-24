5443b0edb64b7a9b0598bfca533a375da7105648
kaingerda <kaingerda123@gmail.com>
2023-07-24 23:26:17 +0300

    web3 integration

---
+++ network.js
@@ -0,0 +1,21 @@
+window.web3ChainId = 5;
+
+window.networks = [
+  {
+    id: 5,
+    label: "Goerli Testnet",
+    token: "ETH",
+    rpcUrl: `https://rpc.ankr.com/eth_goerli`,
+  },
+];
+
+// window.web3ChainId = 1;
+
+// window.networks = [
+//   {
+//     id: 1,
+//     label: "Ethereum Mainnet",
+//     token: "ETH",
+//     rpcUrl: `https://eth.drpc.org`,
+//   },
+// ];
